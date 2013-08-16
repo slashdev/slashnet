e8bfb0a79d859b64322249be1de19c1651eecabb
Ferdi van der Werf <efcm@vdwerf.eu>
2013-08-16 15:14:58 +0200

    Basic main stub

---
+++ src/EthShield.c
@@ -0,0 +1,19 @@
+/**
+ * Copyright (C) Ferdi van der Werf. All rights reserved
+ *
+ * This file is part of EthShield, distributed under the MIT license.
+ */
+
+// Speed of micro-controller (20.0 MHz)
+#ifndef F_CPU
+#define F_CPU 20000000
+#endif
+
+int main(void) {
+    
+    // Infinite loop
+    while (1) {
+        
+    }
+    
+}
