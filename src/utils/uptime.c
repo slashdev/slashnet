2d8e1c9a7df4beb234de2b20e3fb529b201d53bb
Ferdi van der Werf <efcm@vdwerf.eu>
2014-01-04 16:55:10 +0100

    Add skeleton for uptime

---
+++ src/utils/uptime.c
@@ -0,0 +1,16 @@
+/**
+ * @file uptime.c
+ *
+ * \copyright Copyright 2013 /Dev. All rights reserved.
+ * \license This project is released under MIT license.
+ *
+ * @author Ferdi van der Werf <efcm@slashdev.nl>
+ * @since 0.5.0
+ */
+
+#include "uptime.h"
+
+// Do we want uptime?
+#ifdef UTILS_UPTIME
+
+#endif // UTILS_UPTIME
---
+++ src/utils/uptime.h
@@ -0,0 +1,23 @@
+/**
+ * @file uptime.h
+ * @brief Uptime tracker
+ *
+ * This contains the functionality for uptime tracking and periodic calls.
+ *
+ * \copyright Copyright 2013 /Dev. All rights reserved.
+ * \license This project is released under MIT license.
+ *
+ * @author Ferdi van der Werf <efcm@slashdev.nl>
+ * @since 0.5.0
+ */
+
+#ifndef UTILS_UPTIME_H
+#define UTILS_UPTIME_H
+
+#include "../config.h"
+
+// Do we want uptime?
+#ifdef UTILS_UPTIME
+
+#endif // UTILS_UPTIME
+#endif // UTILS_UPTIME_H
