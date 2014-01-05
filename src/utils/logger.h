ca50d9cc4ac6ae46582cb1778a1116e9458f6011
Ferdi van der Werf <efcm@vdwerf.eu>
2014-01-05 16:24:33 +0100

    Introduce info_* and debug_* levels of logging

--- src/utils/logger.h
+++ src/utils/logger.h
@@ -34,10 +34,50 @@ extern void logger__number(uint16_t value);
 extern void logger_number_as_hex(uint16_t value);
 extern void logger_array(uint8_t *data, uint16_t length, char glue);
 
+#if defined(UTILS_LOGGER_INFO)
+
+// Info level enabled
+#define info_string(s) logger_string(s)
+#define info_string_p(s) logger_string_p(s)
+#define info_number(x) logger_number(x)
+#define info_number_as_hex(x) logger_number_as_hex(x)
+#define info_array(x, l) logger_array(x, l)
+// Debug level disabled
+#define debug_string(...) do {} while (0)
+#define debug_string_p(...) do {} while (0)
+#define debug_number(...) do {} while (0)
+#define debug_number_as_hex(...) do {} while (0)
+#define debug_array(...) do {} while (0)
+
+#elif defined(UTILS_LOGGER_DEBUG)
+
+// Info level enabled
+#define info_string(s) logger_string(s)
+#define info_string_p(s) logger_string_p(s)
+#define info_number(x) logger_number(x)
+#define info_number_as_hex(x) logger_number_as_hex(x)
+#define info_array(x, l) logger_array(x, l)
+// Debug level disabled
+#define debug_string(...) do {} while (0)
+#define debug_string_p(...) do {} while (0)
+#define debug_number(...) do {} while (0)
+#define debug_number_as_hex(...) do {} while (0)
+#define debug_array(...) do {} while (0)
+
+#endif // UTILS_LOGGER_INFO
+
+
 #else // UTILS_LOGGER_INFO || UTILS_LOGGER_DEBUG
 
 // No logger wanted, create placeholders
 #define logging_init(...) do {} while (0)
+// Info level disabled
+#define info_string(...) do {} while (0)
+#define info_string_p(...) do {} while (0)
+#define info_number(...) do {} while (0)
+#define info_number_as_hex(...) do {} while (0)
+#define info_array(...) do {} while (0)
+// Debug level disabled
 #define debug_string(...) do {} while (0)
 #define debug_string_p(...) do {} while (0)
 #define debug_number(...) do {} while (0)
