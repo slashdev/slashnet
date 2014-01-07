d5a928f64d5ad7a385e226b9b786de53554edad8
Ferdi van der Werf <efcm@vdwerf.eu>
2014-01-08 00:23:34 +0100

    Define arp layout

--- src/net/arp.h
+++ src/net/arp.h
@@ -21,7 +21,33 @@
 // Do we want ARP?
 #ifdef NET_ARP
 
+#include <inttypes.h>
+#include "network.h"
+#include "shared.h"
+#include "../utils/debug.h"
 
+/**
+ * @brief Initialize the ARP part.
+ *
+ * This includes preparing a cache for replies to arp requests.
+ */
+extern void arp_init(void);
+
+/**
+ * @brief ARP Packet loop handler.
+ *
+ * <i>network_backbone</i> calls this function when it verified an ARP packet is
+ * in <i>buffer_in</i>. This function analyses that packet to see if it's a
+ * request to respond to or a reply to a request which was send.
+ */
+extern void arp_packet_receive(void);
+
+/**
+ * @brief Request the MAC address that corresponds to <i>ip_request</i>.
+ *
+ * @param ip_request The IP address to request the MAC address of
+ */
+extern uint8_t *arp_request_mac(uint8_t *ip_request);
 
 #endif // NET_ARP
 #endif // NET_ARP_H
