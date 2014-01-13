e8c5b66dbde4ba7a0054f8aa9e4cbc1150cd6bf8
Ferdi van der Werf <efcm@vdwerf.eu>
2014-01-13 17:33:43 +0100

    Skeleton for I2C

---
+++ src/com/i2c.c
@@ -0,0 +1,18 @@
+/**
+ * @file i2c.c
+ *
+ * \copyright Copyright 2013 /Dev. All rights reserved.
+ * \license This project is released under MIT license.
+ *
+ * @author Ferdi van der Werf <efcm@slashdev.nl>
+ * @since 0.11.0
+ */
+
+#include "i2c.h"
+
+// Do we want I2C?
+#ifdef COM_I2C
+
+
+
+#endif // COM_I2C
---
+++ src/com/i2c.h
@@ -0,0 +1,26 @@
+/**
+ * @file i2c.h
+ * @brief I2C (TWI) functionality
+ *
+ * Basic functions for communicating with I2C slave devices. This implementation
+ * is limited to a single master on a I2C bus.
+ *
+ * \copyright Copyright 2013 /Dev. All rights reserved.
+ * \license This project is released under MIT license.
+ *
+ * @author Ferdi van der Werf <efcm@slashdev.nl>
+ * @since 0.2.0
+ */
+
+#ifndef COM_I2C_H
+#define COM_I2C_H
+
+#include "../config.h"
+
+// Do we want I2C?
+#ifdef COM_I2C
+
+
+
+#endif // COM_I2C
+#endif // COM_I2C_H
