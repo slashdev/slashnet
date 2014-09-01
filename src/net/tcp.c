dc78a4fe6c104aef0af8f6e1ab27cc1f6a121f85
Ferdi van der Werf <efcm@vdwerf.eu>
2014-09-01 08:21:13 +0200

    Implement reply fin request

--- src/net/tcp.c
+++ src/net/tcp.c
@@ -168,7 +168,14 @@ void reply_syn_request() {
 }
 
 void reply_fin_request() {
-    debug_string_p(PSTR("TODO: Implement reply_fin_request"));
+    // Prepare reply
+    tcp_prepare_reply();
+    // Increase ack with 1
+    add_value_to_buffer(1, &buffer_out[TCP_PTR_ACK_NR], 4);
+    // Set fin and ack flag
+    add_flags(TCP_FLAG_FIN | TCP_FLAG_ACK);
+    // Send packet
+    tcp_send(0);
 }
 
 void tcp_receive(void) {
