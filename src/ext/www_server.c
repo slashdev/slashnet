9df209b1516c25c1749f7fd92300aa606cd65913
Ferdi van der Werf <efcm@vdwerf.eu>
2014-09-14 16:14:09 +0200

    Change indenting to 2 spaces

--- src/ext/www_server.c
+++ src/ext/www_server.c
@@ -27,10 +27,10 @@ void handle_request(uint8_t *data, uint16_t length);
 
 // Path service
 typedef struct {
-    // Path to search for
-    const char *path;
-    // Callback function
-    void (*callback)(uint8_t type, uint8_t *data, uint16_t length);
+  // Path to search for
+  const char *path;
+  // Callback function
+  void (*callback)(uint8_t type, uint8_t *data, uint16_t length);
 } path_service_t;
 
 path_service_t path_services[EXT_WWW_SERVER_SERVICES_LIST_SIZE];
@@ -120,70 +120,67 @@ uint8_t *rbuffer;
 uint16_t rlength;
 
 void handle_request(uint8_t *data, uint16_t length) {
-    debug_string_p(PSTR("HTTP: "));
+  debug_string_p(PSTR("HTTP: "));
 
-    // Prepare variables
-    //uint8_t i;
-
-    uint8_t type = 0;
-    uint8_t path_start = 0;
-    switch(data[0]) {
-        case 'H': type = HTTP_METHOD_HEAD;   path_start = 5; break;
-        case 'G': type = HTTP_METHOD_GET;    path_start = 4; break;
-        case 'D': type = HTTP_METHOD_DELETE; path_start = 7; break;
-        case 'P':
-            switch(data[1]) {
-                case 'O': type = HTTP_METHOD_POST; path_start = 5; break;
-                case 'U': type = HTTP_METHOD_PUT;  path_start = 4; break;
-                default:  type = 0;
-            }
-            break;
-    }
-
-    // Fetch path only if we have a type
-    if (type) {
-        uint8_t path_length = 0;
-        uint8_t *path = &data[path_start];
-        while (*path++ > 0x20) {
-            path_length++;
-        }
-        // Make space after path 0x00 (for path matching)
-        data[path_start + path_length] = 0x00;
-        // Debug path
-        debug_string_n((char *)data, path_start + path_length);
-    }
-
-    // Get callback for type/path combination
-    // path_services_get(&data[path_start]);
-    uint8_t i;
-    void (*callback)(uint8_t, uint8_t *, uint16_t);
-
-    for (i = 0, callback = 0; i < EXT_WWW_SERVER_SERVICES_LIST_SIZE; i++) {
-      if (path_services[i].path && path_matches(&data[path_start], path_services[i].path)) {
-        callback = path_services[i].callback;
-        break;
+  uint8_t type = 0;
+  uint8_t path_start = 0;
+  switch(data[0]) {
+    case 'H': type = HTTP_METHOD_HEAD;   path_start = 5; break;
+    case 'G': type = HTTP_METHOD_GET;    path_start = 4; break;
+    case 'D': type = HTTP_METHOD_DELETE; path_start = 7; break;
+    case 'P':
+      switch(data[1]) {
+        case 'O': type = HTTP_METHOD_POST; path_start = 5; break;
+        case 'U': type = HTTP_METHOD_PUT;  path_start = 4; break;
+        default:  type = 0;
       }
+      break;
+  }
+
+  // Fetch path only if we have a type
+  if (type) {
+    uint8_t path_length = 0;
+    uint8_t *path = &data[path_start];
+    while (*path++ > 0x20) {
+      path_length++;
     }
+    // Make space after path 0x00 (for path matching)
+    data[path_start + path_length] = 0x00;
+    // Debug path
+    debug_string_n((char *)data, path_start + path_length);
+  }
 
-    // Prepare tcp reply
-    rbuffer = tcp_prepare_reply();
-    rlength = 0;
-    // Increase ack with length
-    add_value_to_buffer(length, &buffer_out[TCP_PTR_ACK_NR], 4);
-    // Set ACK flag
-    tcp_add_flags(TCP_FLAG_ACK | TCP_FLAG_PUSH | TCP_FLAG_FIN);
+  // Get callback for type/path combination
+  // path_services_get(&data[path_start]);
+  uint8_t i;
+  void (*callback)(uint8_t, uint8_t *, uint16_t);
 
-    // Check if we can handle the request
-    if (callback) {
-      callback(type, data, 0);
-    } else {
-        // Return 404, not found
-        www_server_reply_header(HTTP_STATUS_404, HTTP_CONTENT_TYPE_PLAIN);
-        www_server_buffer_add_p(not_found);
-        www_server_reply_send();
+  for (i = 0, callback = 0; i < EXT_WWW_SERVER_SERVICES_LIST_SIZE; i++) {
+    if (path_services[i].path && path_matches(&data[path_start], path_services[i].path)) {
+      callback = path_services[i].callback;
+      break;
     }
+  }
 
-    debug_ok();
+  // Prepare tcp reply
+  rbuffer = tcp_prepare_reply();
+  rlength = 0;
+  // Increase ack with length
+  add_value_to_buffer(length, &buffer_out[TCP_PTR_ACK_NR], 4);
+  // Set ACK flag
+  tcp_add_flags(TCP_FLAG_ACK | TCP_FLAG_PUSH | TCP_FLAG_FIN);
+
+  // Check if we can handle the request
+  if (callback) {
+    callback(type, data, 0);
+  } else {
+    // Return 404, not found
+    www_server_reply_header(HTTP_STATUS_404, HTTP_CONTENT_TYPE_PLAIN);
+    www_server_buffer_add_p(not_found);
+    www_server_reply_send();
+  }
+
+  debug_ok();
 }
 
 const char http_version[] PROGMEM = "HTTP/1.1 ";
@@ -200,64 +197,64 @@ const char http_content_type_html[]  PROGMEM = "text/html";
 const char http_content_type_json[]  PROGMEM = "application/json";
 
 void www_server_reply_header(uint8_t status, uint8_t content_type) {
-    // HTTP version
-    www_server_buffer_add_p(http_version);
+  // HTTP version
+  www_server_buffer_add_p(http_version);
 
-    // Status
-    if (0) {}
-    else if (status == HTTP_STATUS_200) { www_server_buffer_add_p(http_status_200); }
-    else if (status == HTTP_STATUS_201) { www_server_buffer_add_p(http_status_201); }
-    else if (status == HTTP_STATUS_202) { www_server_buffer_add_p(http_status_202); }
-    else if (status == HTTP_STATUS_204) { www_server_buffer_add_p(http_status_204); }
-    else if (status == HTTP_STATUS_404) { www_server_buffer_add_p(http_status_404); }
+  // Status
+  if (0) {}
+  else if (status == HTTP_STATUS_200) { www_server_buffer_add_p(http_status_200); }
+  else if (status == HTTP_STATUS_201) { www_server_buffer_add_p(http_status_201); }
+  else if (status == HTTP_STATUS_202) { www_server_buffer_add_p(http_status_202); }
+  else if (status == HTTP_STATUS_204) { www_server_buffer_add_p(http_status_204); }
+  else if (status == HTTP_STATUS_404) { www_server_buffer_add_p(http_status_404); }
 
-    // Newline
-    www_server_buffer_add_p(newline);
+  // Newline
+  www_server_buffer_add_p(newline);
 
-    // Headers
-    // --------------------------------------------------------------------
+  // Headers
+  // --------------------------------------------------------------------
 
-    // Content type
-    www_server_buffer_add_p(http_content_type_head);
-    if (0) {
-    } else if (content_type == HTTP_CONTENT_TYPE_PLAIN) {
-        www_server_buffer_add_p(http_content_type_plain);
-    } else if (content_type == HTTP_CONTENT_TYPE_HTML) {
-        www_server_buffer_add_p(http_content_type_html);
-    } else if (content_type == HTTP_CONTENT_TYPE_JSON) {
-        www_server_buffer_add_p(http_content_type_json);
-    }
-    www_server_buffer_add_p(newline);
+  // Content type
+  www_server_buffer_add_p(http_content_type_head);
+  if (0) {
+  } else if (content_type == HTTP_CONTENT_TYPE_PLAIN) {
+      www_server_buffer_add_p(http_content_type_plain);
+  } else if (content_type == HTTP_CONTENT_TYPE_HTML) {
+      www_server_buffer_add_p(http_content_type_html);
+  } else if (content_type == HTTP_CONTENT_TYPE_JSON) {
+      www_server_buffer_add_p(http_content_type_json);
+  }
+  www_server_buffer_add_p(newline);
 
-    // End header with extra newline
-    www_server_buffer_add_p(newline);
+  // End header with extra newline
+  www_server_buffer_add_p(newline);
 }
 
 void www_server_reply_send() {
-    // Make sure a packet ends with two newlines to mark end of data
-    www_server_buffer_add_p(newline);
-    www_server_buffer_add_p(newline);
-    // Send packet
-    tcp_send(rlength);
+  // Make sure a packet ends with two newlines to mark end of data
+  www_server_buffer_add_p(newline);
+  www_server_buffer_add_p(newline);
+  // Send packet
+  tcp_send(rlength);
 }
 
 void www_server_buffer_add(char *data) {
-    www_server_buffer_add_n(data, 0xFFFF);
+  www_server_buffer_add_n(data, 0xFFFF);
 }
 
 void www_server_buffer_add_n(char *data, uint16_t length) {
-    while(*data && length--) {
-        *rbuffer++ = *data++;
-        rlength++;
-    }
+  while(*data && length--) {
+    *rbuffer++ = *data++;
+    rlength++;
+  }
 }
 
 void www_server_buffer_add_p(const char *pdata) {
-    char c;
-    while ((c = pgm_read_byte(pdata++))) {
-        *rbuffer++ = c;
-        rlength++;
-    }
+  char c;
+  while ((c = pgm_read_byte(pdata++))) {
+    *rbuffer++ = c;
+    rlength++;
+  }
 }
 
 #endif // EXT_WWW_SERVER
